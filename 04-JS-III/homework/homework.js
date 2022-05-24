d8c7bd2933c1d6b21eca35f3ac8f0d9e6f9464e1
cash-alan <cash.alanbo2@gmail.com>
2022-05-24 17:18:22 +0200

    arrays isn't easy

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,21 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array.shift();
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array.pop();
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
 }
 
 
@@ -23,6 +26,10 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  for (let i = 0; i < array.length; i++) {
+    array[i] += 1;
+  }
+  return array;
 }
 
 
@@ -30,6 +37,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento);
+  return array;
 }
 
 
@@ -38,6 +47,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento);
+  return array;
 }
 
 
@@ -47,13 +58,19 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  let frase = ''; /*sirve para guardar la concatenacion*/
+  for (let i = 0; i < palabras.length;i++) {
+    frase += (i === 0) ? palabras[i] : (' ' + palabras[i]);/*para dar los espacios entre palabras(es otra forma de dar condiciones)*/
+  }
+  return frase;/*devuelva la frase*/
 }
 
 
 function arrayContiene(array, elemento) {
-  // Comprueba si el elemento existe dentro de "array"
+  //Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  return array.includes(elemento);
 }
 
 
@@ -61,20 +78,30 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  let suma = undefined;
+  for (let i = 0; i < numeros.length; i++) {
+    (i === 0) ? suma = numeros[i] : suma += numeros[i];
+  }
+  return suma;
 }
-
 
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
-}
 
+    let suma = undefined;
+    for (let i = 0; i < resultadosTest.length; i++) {
+      (i === 0) ? suma = resultadosTest[i] : suma += resultadosTest[i];
+    }
+    return suma / resultadosTest.length;
+}
 
 function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  return Math.max(...numeros);
 }
 
 
@@ -82,13 +109,25 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  let mult = undefined;
+  if (arguments.length === 0) {
+    return 0;
+  } else if (arguments.length === 1) {
+    return arguments[0];
+  } else {
+    for (let i = 0; i < arguments.length; i++){
+      (i === 0) ? mult = arguments[i] : mult *= arguments[i];
+    }
+    return mult;
+  } 
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  cantidad = arreglo.filter( element => element > 18);
+  return cantidad.length;
 }
 
 
@@ -96,8 +135,12 @@ function diaDeLaSemana(numeroDeDia) {
   //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
-  //Escribe tu código aquí   
-  
+  //Escribe tu código aquí
+  if (numeroDeDia === 1 || numeroDeDia === 7){
+    return 'Es fin de semana';
+  } else {
+    return 'Es dia Laboral';
+  }
 } 
 
 
@@ -105,7 +148,7 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  return /^9/.test(n);/*revisa https://es.stackoverflow.com/questions/107327/comparar-el-primer-caracter-del-texto sino recuerdas*/
 }
 
 
@@ -113,7 +156,19 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  let igualdad = undefined;
+  for (let i = 0; i < arreglo.length; i++){
+    if (arreglo[i] === arreglo[0]){
+      igualdad = true;
+    } else {
+      igualdad = false;
+    }
+  }
+  if (igualdad === true){
+    return true;
+  } else {
+    return false;
+  }
 } 
 
 
@@ -121,7 +176,14 @@ function mesesDelAño(array) {
   //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de 
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
-  // Tu código:
+  // // Tu código:
+  let newarray = array.filter(element => element === 'Marzo' || element ==='Noviembre' || element === 'Enero');
+
+  if (newarray.includes('Enero') && newarray.includes('Marzo') && newarray.includes('Noviembre')){
+    return newarray;
+  } else {
+    return 'No se encontraron los meses pedidos';
+  }
 }
 
 
@@ -129,6 +191,8 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  let newarray = array.filter(element => element > 100);
+  return newarray;
 }
 
 
@@ -140,6 +204,18 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  let numeronew = [];
+  for (let i = 0; i < 10; i++) {
+    numeronew.push(numero += 2);
+    if (i === (numero + 2)) { 
+      break;
+    }
+  }
+  if (numeronew.length === 10) {
+    return numeronew;
+  } else {
+    return 'Se interrumpió la ejecución';
+  }  
 }
 
 
@@ -150,6 +226,14 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  let numeronew = [];
+  for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+      continue;
+    }
+    numeronew.push(numero += 2);
+  }
+  return numeronew;  
 }
 
 
--- JS_cashalan_notes/comands.md
+++ JS_cashalan_notes/comands.md
@@ -1,37 +1,53 @@
 ```js
 // data
-var /*<variable_nombre>*/ = /*<variable_valor>*/; : sirve para declarar variables
-const /*<nombre_constante>*/ = /*<constante_valor>*/; : sirve para declarar constantes
-/*<dato>*/ /*<operador>*/ /*<dato>*/ : sirve para hacer operaciones
+var /*variable_nombre*/ = /*variable_valor*/; : sirve para declarar variables
+const /*nombre_constante*/ = /*constante_valor*/; : sirve para declarar constantes
+/*dato*/ /*operador*/ /*dato*/ : sirve para hacer operaciones
 // operadores
-/*<dato>*/ + /*<dato>*/ : suma o concatena
-/*<dato>*/ - /*<dato>*/ : resta 
-/*<dato>*/ * /*<dato>*/ : multiplica
-/*<dato>*/ / /*<divide>*/ : divide
-/*<dato>*/ % /*<divide>*/ : divide y devuelve el residuo
-/*<dato>*/ < /*<divide>*/ : compara si el primero es menor que el segundo
-/*<dato>*/ > /*<divide>*/ : compara si el primero es mayor que el segundo
-/*<dato>*/ == /*<divide>*/ : compara si el primero es igual que el segundo
-/*<dato>*/ === /*<divide>*/ : compara si el primero es igual que el segundo en todo
-/*<dato*/++ : sumara uno al dato es igual a (/*dato*/ + 1;)
+/*dato*/ + /*dato*/ : suma o concatena
+/*dato*/ - /*dato*/ : resta 
+/*dato*/ * /*dato*/ : multiplica
+/*dato*/ / /*divide*/ : divide
+/*dato*/ % /*divide*/ : divide y devuelve el residuo
+/*dato*/  /*divide*/ : compara si el primero es menor que el segundo
+/*dato*/  /*divide*/ : compara si el primero es mayor que el segundo
+/*dato*/ == /*divide*/ : compara si el primero es igual que el segundo
+/*dato*/ === /*divide*/ : compara si el primero es igual que el segundo en todo
+/*dato*/++ : sumara uno al dato es igual a (/*dato*/ + 1;)
 // function 
-function  /*<nombre_funcion>(/*<argumentos>) {/*</*codigo>*/ : sirve para crear funciones
-const /*<nombre_funcion> = function () {/*</*codigo>*/; : sirve para crear funciones dentro de variables (fijate que se nombra la variable no la funcion)
-return /*<>*/; : sirve para retornar valores
-//hay funciones ya predeterminadas como
+function  /*nombre_funcion(/*argumentos) {/*/*codigo*/ : sirve para crear funciones
+const /*nombre_funcion = function () {/*/*codigo*/; : sirve para crear funciones dentro de variables (fijate que se nombra la variable no la funcion)
+(/*parametros*/) = {/*codigo*/;} : es una forma resumida de crear funciones anonimas(sin nombre),si la funcion solo tiene una linea de codigo y/o un parametro puedes omitir las llaves y/o los parentesis (quedando algo asi /*parametro*/=/*codigo*/;)
+return /**/; : sirve para retornar valores
+//hay funciones ya predeterminadas llamadas metodos como
 console.log('argumento') : sirve para imprimir cualquier dato que le indiques por argumento
 alert('argumento') : muestra una ventana de alerta con el argumento que le des
 prompt('argumento') : muestra una ventana de alerta con el argumento que le des pidiendo datos al usuario
-Math./*<method>*/() : son algunas operaciones matematicas
+Math./*method*/() : son algunas operaciones matematicas
+Math.min() : Para encontrar el valor mínimo presente
+Math.max() : Para encontrar el valor máximo presente
 // condicionals
 if (condicion) {} : sirve para crear el condicional
 else {} : se escribe al final y se ejecuta cuando todo lo demas es falso (va siempre al final)
 else if (condicion) {} : es como sino con condicion como un entonces (va siempre despues del if)
 switch(n){case n_valor:/*codigo*/case n_valor:/*codigo*/ default(opcional)} : sirve para dar varias condiciones
-return /*<codigo>*/; : sirve para retornar valores y termina de ejecutar el codigo cuando se enuncie
+return /*codigo*/; : sirve para retornar valores y termina de ejecutar el codigo cuando se enuncie
 break; : obliga a terminar el codigo
 // loops
-for (/*<variable>*/;/*<condicion>*/;/*<accion>*/) {} : sirve para crear bucles
-while(/*<condicion>*/) {} : sirve para crear bucles sin saber que cantidad de veces se repite
-do{/*codigo*/}while(/*<condicion>*/) : sirve para crear bucles sin saber que cantidad de veces se repite pero el do siempre lo va a ejecutar una vez
+for (/*variable_contador*/;/*condicion_true*/;/*accion/acumulacion*/) {/*codigo*/} : sirve para crear iteraciones
+for (/*variable_elemento*/ of /*objeto*/){/*codigo*/} : sirve para iterar en objetos(arrays,strings,maps,...), equivalentes a for (let i=0;iarray.length;i++){console.log(array[i]);} ===  for (let e  of array){console.log(e);}, como ejemplo 
+while(/*condicion*/) {} : sirve para crear bucles sin saber que cantidad de veces se repite
+do{/*codigo*/}while(/*condicion*/);  : sirve para crear bucles sin saber que cantidad de veces se repite pero el do siempre lo va a ejecutar una vez
+// arrays
+[/*datos*/] : es un array
+array.push() : envia datos al final del array
+array.pop() : elimina y trae el ultimo dato del array
+array.unshift() : colocará un nuevo elemento en la primera posición de la matriz 
+array.shift() : elimina y trae el primer elemento de la matriz
+array.lenght() : nos dice cuantos valores tiene un array
+array.map() : toma cada elemento del arreglo y lo usa en una función para devolver un nuevo valor en la misma posición de un nuevo arreglo.
+array.filter(): para encontrar los elementos dentro de un arreglo que cumplan con cierta condición
+array.indexOf() : devuelve el primer índice encontrado de un elemento específico
+array.includes() : determina si un arreglo incluye un valor específico y devuelve verdadero o falso según corresponda
+array.find() : para encontrar el primer elemento que cumple cierta condición
 ```
